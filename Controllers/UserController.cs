4440ee521d4395f71f1822d55a2ad7d17ae856ef
Đỗ Đăng Huy <sieunhan482@gmail.com>
2025-02-15 14:00:25 +0700

    add models entites

---
+++ Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using electro_shop_backend.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace electro_shop_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController
+    {
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return new List<User>
+            {
+                new User { Id = 1, Name = "John Doe" },
+                new User { Id = 2, Name = "Jane Doe" },
+            };
+        }
+    }
+}
---
+++ Models/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace electro_shop_backend.Models.Entities
+{
+    public class User : IdentityUser
+    {
+        public int user_id { get; set; }
+        [Required]
+        public string username { get; set; }
+        [Required]
+        public string password { get; set; }
+        public string fullname { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string avatar_img { get; set; }
+        [Required]
+        public string role { get; set; }
+        [Required]
+        public string user_status { get; set; }
+        public DateTime created_at { get; set; }
+    }
+}
