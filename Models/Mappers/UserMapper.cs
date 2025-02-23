ef759f18b248a1a3bbb883cb15f9e5c36654b22e
Đỗ Đăng Huy <sieunhan482@gmail.com>
2025-02-23 22:19:07 +0700

    add UserMapper

---
+++ Models/Mappers/UserMapper.cs
@@ -0,0 +1,33 @@
+﻿using electro_shop_backend.Models.DTOs.User;
+using electro_shop_backend.Models.Entities;
+
+namespace electro_shop_backend.Models.Mappers
+{
+    public static class UserMapper
+    {
+        public static ViewUserForAdminDTO ToViewUserForAdminDTOFromUser (this User user)
+        {
+            return new ViewUserForAdminDTO ()
+            {
+                UserName = user.UserName,
+                FullName = user.FullName,
+                Address = user.Address,
+                Email = user.Email,
+                EmailConfirmed = user.EmailConfirmed,
+                PhoneNumber = user.PhoneNumber,
+                AvatarImg = user.AvatarImg,
+                UserStatus = user.UserStatus,
+                CreatedAt = user.CreatedAt
+            };      
+        }
+
+        public static NewUserDTO ToNewUserDTOFromUser (this User user)
+        {
+            return new NewUserDTO()
+            {
+                UserName = user.UserName,
+                Email = user.Email
+            };
+        }
+    }
+}
