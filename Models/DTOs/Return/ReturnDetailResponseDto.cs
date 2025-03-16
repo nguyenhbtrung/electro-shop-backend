5fda296b9896d07d603119d0f9439ac6fc499a19
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-03-16 22:13:38 +0700

    add Get Return By Id api

---
+++ Models/DTOs/Return/ReturnDetailResponseDto.cs
@@ -0,0 +1,16 @@
+﻿
+namespace electro_shop_backend.Models.DTOs.Return
+{
+    public class ReturnDetailResponseDto
+    {
+        public int ReturnId { get; set; }
+        public string? Reason { get; set; }
+        public string? Detail { get; set; }
+        public string? Status { get; set; }
+        public string? ReturnMethod { get; set; }
+        public string? AdminComment { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public List<ReturnHistoryDto> ReturnHistories { get; set; } = [];
+        public List<ReturnProductDto> ReturnProducts { get; set; } = [];
+    }
+}
