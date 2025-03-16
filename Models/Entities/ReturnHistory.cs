0e59825f24b59c43f19dad7110d0493fe25047e3
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-03-16 17:05:10 +0700

    create Return history table

---
+++ Models/Entities/ReturnHistory.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace electro_shop_backend.Models.Entities
+{
+    [Table("Return_History")]
+    public class ReturnHistory
+    {
+        [Key]
+        [Column("return_history_id")]
+        public int ReturnHistoryId { get; set; }
+        [Column("return_id")]
+        public int ReturnId { get; set; }
+
+        [Column("status")]
+        [StringLength(50)]
+        public string? Status { get; set; }
+
+        [Column("changed_at", TypeName = "datetime")]
+        public DateTime? ChangedAt { get; set; }
+        [ForeignKey("ReturnId")]
+        [InverseProperty("ReturnHistories")]
+        public virtual Return? Return { get; set; }
+
+    }
+}
